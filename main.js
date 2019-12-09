448569a80337486a756c9ca251afd70bbf1a9578
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-09 14:26:05 -0500

    add to start solution

--- main.js
+++ main.js
@@ -9,16 +9,38 @@ const Lil = () => {
   return {
     head: null,
 
-    values: function(value) {
-      // value: value,
-      // next : null
+    values: function() {
+      const nodeArr = [];
+      let current = this.head;
+      while (current !== null){
+        
+        nodeArr.push(current.value); 
+        current = current.next
+      }
+      
+      return nodeArr;
     },
 
     addToStart: function(value) {
-
+      startNode = Node(value);
+      if (this.head === null){
+        this.head = startNode;
+      }
+      else{
+        let oldNode = this.head;
+        startNode.next = oldNode
+        this.head = startNode;
+      }
+      this.length ++
+      return this
     },
 
     addToEnd: function(value) {
+      endNode = Node(value);
+      if (this.head === null){
+        this.head = endNode;
+      }
+      else{}
     
     },
 
