780af4c6994f52ea2270514e6e74bfbb0c60059e
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-09 15:49:07 -0500

    missing last one

--- main.js
+++ main.js
@@ -25,14 +25,12 @@ const Lil = () => {
       startNode = Node(value);
       if (this.head === null){
         this.head = startNode;
-      }
-      else{
+      } else {
         let oldNode = this.head;
         startNode.next = oldNode
         this.head = startNode;
       }
-      this.length ++
-      return this
+    
     },
 
     addToEnd: function(value) {
@@ -40,28 +38,76 @@ const Lil = () => {
       if (this.head === null){
         this.head = endNode;
       }
-      else{}
-    
+
+      else if (this.head !== null){
+        let current = this.head
+        while(current.next){
+          current = current.next
+        }
+        current.next= endNode
+        
+      } 
     },
 
     removeFromStart: function() {
-    
+      const old = this.head;
+      this.head = old.next;
+      return old.value;
     },
 
     removeFromEnd: function() {
-    
+      if (this.head !== null){
+        let current = this.head
+        while(current.next.next){
+          current = current.next
+        }
+
+        let oldValue =current.next
+        current.next = null;
+        
+        return oldValue.value
+      }
+      else{
+        return null
+      }
     },
+    
 
     getAt: function(i) {
-    
+      let current = this.head;
+      if (current === null){
+        return null
+      }
+      else{
+      newArr = this.values();
+      return newArr[i];
+      }
     },
 
     removeAt: function(i) {
-    
+      let current = this.head;
+      if (current === null){
+        return null
+      }
+      else{
+        newArr = this.values();
+        newArr[i] = 0;
+        return newArr[i];
+      }
     },
   }
 }
 
+const test = Lil();
+
+test.addToEnd(5)
+test.addToEnd(3)
+
+test.removeFromEnd()
+test.addToEnd(15)
+test.removeFromEnd()
+const result = test.values()
+result
 
 module.exports = {
   Lil,
